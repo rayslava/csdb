c0930575ad65a4e0c37253e0348882a0afe2ae24
Slava Barinov <rayslava@gmail.com>
2019-12-27 15:58:30 +0300

    compiler defines added

---
+++ include/tools/compiler.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GCC_BUILD (defined(__GNUC__) && !defined(__clang__))
+
+#ifdef GCC_BUILD
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+#else
+#define likely(x)      (x)
+#define unlikely(x)    (x)
+#endif
